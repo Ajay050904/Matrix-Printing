d1123a03b67bc6da6088c269fd4da44a0c6007d8
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2023-09-11 23:46:42 +0530

    Update DisplayBelowDiagonalEle2.java

--- DisplayBelowDiagonalEle2.java
+++ DisplayBelowDiagonalEle2.java
@@ -55,7 +55,7 @@ public class DisplayBelowDiagonalEle2 {
 /*
  * O(1) read as bigo of 1-
  * ***********************
- * 							// time-complexity : O(m*n)
-						    // space complexity : O(1) 						
+ * 			   // time-complexity : O(m*n)
+			   // space complexity : O(1) 						
  */
 
