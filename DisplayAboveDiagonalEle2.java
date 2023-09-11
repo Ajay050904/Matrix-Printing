caea3f0dd0a41c37622bfe15278cbcc1dd25e087
Ajay Vishwakarma <143311082+Ajay050904@users.noreply.github.com>
2023-09-11 23:37:18 +0530

    Add files via upload

---
+++ DisplayAboveDiagonalEle2.java
@@ -0,0 +1,58 @@
+package Arrays;
+
+import java.util.Scanner;
+
+// Display all the elements above primary diagonal 
+
+/*
+ * Input -
+ * *******
+  			1 2 3 5
+   			4 5 6 4
+   			7 8 9 1
+   			4 6 2 8
+ * Output -
+ * ********
+ 			 2 3 5
+ 			   6 4
+ 			     1
+ 			     
+ */
+
+public class DisplayAboveDiagonalEle2 {
+	public static void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows for creating 2-d array : ");
+		int n = sc.nextInt();
+		System.out.print("Enter the number of columns for creating 2-d array : ");
+		int m = sc.nextInt();
+		// 2-D array initialization
+		int[][] arr = new int[n][m];
+		System.out.println("Enter the array elements : ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (i < j) { 							
+					System.out.print(arr[i][j] + " ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
+
+/*
+ * O(1) read as bigo of 1-
+ * ***********************
+ * 							// time-complexity : O(m*n)
+						    // space complexity : O(1) 						
+ */
+
+
---
+++ DisplayBelowDiagonalEle2.java
@@ -0,0 +1,61 @@
+package Arrays;
+
+import java.util.Scanner;
+
+// Display all the elements below primary diagonal 
+
+/*
+ * Input -
+ * *******
+  			1 2 3 5
+   			4 5 6 4
+   			7 8 9 1
+   			4 6 2 8
+ * Output -
+ * ********
+ 	
+ 			4 
+ 			7 8 
+ 			4 6 2
+ */
+
+public class DisplayBelowDiagonalEle2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of rows for creating 2-d array : ");
+		int n = sc.nextInt();
+		System.out.print("Enter the number of columns for creating 2-d array : ");
+		int m = sc.nextInt();
+		// 2-D array initialization
+		int[][] arr = new int[n][m];
+		System.out.println("Enter the array elements : ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+			arr[i][j] = sc.nextInt();
+		}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+			if( i > j ) {
+				System.out.print(arr[i][j] + " ");
+			}
+			else {
+				System.out.print(" ");
+			}
+		}
+			System.out.println();
+		}
+		sc.close();
+
+	}
+
+}
+
+/*
+ * O(1) read as bigo of 1-
+ * ***********************
+ * 							// time-complexity : O(m*n)
+						    // space complexity : O(1) 						
+ */
+
